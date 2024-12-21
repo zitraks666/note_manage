2513a2d846769f68ad575679d220ca74bd69a744
akari <aidv2ar@gmail.com>
2024-12-21 22:21:18 +0300

    part2

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from datetime import *
 
-today_date=input('Введите сегодняшнюю дату(день-месяц-год): ')
-today_date=datetime.strptime(today_date, '%d-%m-%Y').date()
 def note():
     username = input('Введите имя: ')
     title = input('Введите заголовок: ')
     content = input('Напишите заметку: ')
     created_day=input('Введите дату создания(день-месяц-год): ')
-    created_day = datetime.strptime(created_day, '%d-%m-%Y').date()
+    temp_created_date=created_day[0:5]
     issue_date = input('Введите дедлайн(день-месяц-год): ')
-    issue_date=datetime.strptime(issue_date, '%d-%m-%Y').date()
-    if today_date>issue_date:
-        status=' НЕ Актуально'
-    else:
-        status='Актуально'
-
-    print(status)
+    temp_issue_date=created_day[0:5]
+    print(f'\nВаше имя: {username} \nЗаголовок заметки: {title} \n аметка: {content} \nДата создания:{temp_created_date} \nДедлайн:{temp_issue_date}', sep='\n')
 note()
+
 
 
 
